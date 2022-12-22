10d48e2546816da6cfe6c9a73522615c966891f3
Jahongir2109 <jahongirachilov004@gmail.com>
2022-12-22 15:14:13 +0500

    aliquotNumber

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character=='A' || character=='a' || character=='I' || character=='i'){
+            System.out.println("Vowel");
+        } else if (character == 'E' || character == 'e' || character=='O' || character=='o' || character=='U'||character=='u') {
+            System.out.println("Vowel");
+        }else {
+            System.out.println("Consonant");
+        }
     }
 }

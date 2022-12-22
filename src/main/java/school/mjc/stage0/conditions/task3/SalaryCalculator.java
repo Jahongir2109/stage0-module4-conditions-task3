ed2c06126cad79c29579e433deddcc3a0fd228e1
Jahongir2109 <jahongirachilov004@gmail.com>
2022-12-22 15:40:32 +0500

    fixed

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,13 +1,16 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+    public static void main(String[] args) {
+       calculateSalary(5000);
+    }
+    public static void calculateSalary(int salary) {
         if(salary>0 && salary<=10000){
-            System.out.println(salary-(salary/100)*15);
+            System.out.println(salary-(double)(salary/100)*15);
         } else if (salary > 10000 && salary <= 20000) {
-            System.out.println(salary-(salary/100*18));
+            System.out.println(salary-(double)(salary/100*18));
         } else if (salary>20000) {
-            System.out.println(salary-(salary / 100 * 20));
+            System.out.println(salary-(double)(salary / 100 * 20));
         } else {
             System.out.println("wrong input!");
         }

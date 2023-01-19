bb17a6002d515054873236b6ed6233e2798be123
Jahongir2109 <jahongirachilov004@gmail.com>
2023-01-19 12:48:29 +0500

    fixed english alphabet

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
-        if(symbol=='E' || symbol=='e'){
+        if((symbol >= 65 && symbol<=90) || (symbol>=97 && symbol<=122)){
             System.out.println("English");
         }else {
             System.out.println("Non English");
